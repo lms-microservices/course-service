00416d5f58783b5ff6b5e546137a8eda7482acfe
YoussefZahran2005 <youssef.assem.zahran@gmail.com>
2026-04-29 07:19:32 +0300

    feat: added validation to the DTOs

--- src/main/java/com/lms/course/dto/UpdateCourseRequest.java
+++ src/main/java/com/lms/course/dto/UpdateCourseRequest.java
@@ -1,14 +1,27 @@
 package com.lms.course.dto;
 
 import com.lms.course.enums.DifficultyLevel;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class UpdateCourseRequest {
+
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price cannot be negative")
     private Double price;
+
+    @NotBlank(message = "Category is required")
     private String category;
+
     private String thumbnailUrl;
+
+    @NotNull(message = "Difficulty level is required")
     private DifficultyLevel difficultyLevel;
 }
